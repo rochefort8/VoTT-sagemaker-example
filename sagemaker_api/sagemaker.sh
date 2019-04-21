aa9ae8df6efc958811a5427b5c3c3813f218f110
Yuji Ogihara <yuji.ogihara.85@gmail.com>
2019-04-21 21:16:29 +0900

    Added Sagemaker API scripts

---
+++ sagemaker_api/sagemaker.sh
@@ -0,0 +1,1 @@
+aws sagemaker-runtime invoke-endpoint --endpoint-name am --body fileb:////Users/rochefort10/projects/analog_meter/a/Gauge/gauge1.jpg --content-type application/x-image ./output1.json
